00044f597123215e2884287317f5b8512f7fd41c
Tino Muzambi <tinomuzambi@gmail.com>
2021-10-28 22:50:08 +0200

    Adds changing arrow.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ const answers = document.querySelectorAll("div.question-item");
 console.log(answers);
 answers.forEach((answer) =>
 	answer.addEventListener("click", () => {
+		const arrow = answer.children[0].children[1].innerText;
+		if (arrow === "▲") answer.children[0].children[1].innerText = "▼";
+		else answer.children[0].children[1].innerText = "▲";
 		answer.children[1].classList.toggle("open");
 	})
 );
