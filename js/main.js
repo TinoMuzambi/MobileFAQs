7ca43b54bb981dc871150771db2f791c8932165b
Tino Muzambi <tinomuzambi@gmail.com>
2021-10-28 22:40:01 +0200

    Adds toggler.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
-const rows = document.querySelectorAll(".row");
-const answers = document.querySelectorAll(".answer");
+const answers = document.querySelectorAll("div.question-item");
+
+console.log(answers);
+answers.forEach((answer) =>
+	answer.addEventListener("click", () => {
+		answer.children[1].classList.toggle("open");
+	})
+);
